524354d671bac859d5295bbf2c383c861a904349
Nikolay Valchev <nivalchev@gmail.com>
2017-10-19 11:17:59 +0300

    Enable CORS on the Server

--- server/server.mjs
+++ server/server.mjs
@@ -2,23 +2,25 @@ import http from 'http'
 import express from 'express'
 
 import * as routes from './routes'
+import { cors } from './middlewares';
 
 export default class Server {
-  constructor () {
+  constructor() {
     this.app = express();
 
+    this.app.use(cors);
     this.app.use('/status', this.status);
     this.app.use('/domains', routes.domains);
   }
 
-  async start (port = 3000) {
+  async start(port = 3000) {
     const server = await http.createServer(this.app);
     return server.listen(port, () => {
       console.info('🚀 Server is running.');
     });
   }
 
-  status (req, res) {
+  status(req, res) {
     res.sendStatus(200);
   }
 }
